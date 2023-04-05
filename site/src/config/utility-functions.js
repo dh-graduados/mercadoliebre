58b8137a60da7a69849ec9c9aa25cdd6e5caf2c7
Pablo Baleztena <pablo@ultharsoftware.com>
2023-04-05 18:47:24 -0300

    Add utility function to print prices

--- site/src/config/utility-functions.js
+++ site/src/config/utility-functions.js
@@ -1,13 +1,15 @@
+function printPrice(value) {
+    return value.toLocaleString("es-ar", {
+        minimumFractionDigits: 2,
+    });
+}
 module.exports = {
     config(app) {
         app.locals.getProductPrice = function (product) {
-            return (product.price - product.price * (product.discount / 100)).toLocaleString(
-                "es-ar",
-                {
-                    minimumFractionDigits: 2,
-                }
-            );
+            return printPrice(product.price - product.price * (product.discount / 100));
         };
+
+        app.locals.printPrice = printPrice;
 
         app.locals.getOldValue = (locals, key) => {
             return locals.oldData && locals.oldData[key] ? locals.oldData[key] : "";
